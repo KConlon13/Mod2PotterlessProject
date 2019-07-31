564e2d76bcf73264fe39734d405688067e3452d7
KConlon13 <KyleConlon13@gmail.com>
2019-07-31 09:25:45 -0400

    fixed bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,15 @@
 class SessionsController < ApplicationController
 
   def new
-    ##something should go here or we should changes the render below
   end
 
   def create
     @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(password: params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      flash[:message] = "Invalid Username/Password Combination"
+      flash.now[:message] = "Invalid Username/Password Combination"
       render :new 
     end
   end
