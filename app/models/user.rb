ac6aa4bbb5c388b88b457efaaa3b08444adabcf2
KConlon13 <KyleConlon13@gmail.com>
2019-07-31 11:17:04 -0400

    fixed delete user profile issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   belongs_to :house
-  has_many :spells, through: :castings
+  has_many :castings, :dependent => :destroy
+  has_many :spells, through: :castings, dependent: :destroy
   has_one :patronu, :dependent => :destroy
   has_secure_password
   validates :name, presence: true
